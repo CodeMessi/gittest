8bd7e432195adb3d9e132e49a00073578504f9ea
Layne <Layne@atguigu.com>
2021-08-13 13:43:11 +0800

    second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -3,7 +3,9 @@ package com.atguigu.git;
 public class GitTest {
 
     public static void main(String[] args) {
-        System.out.println("hello gir");
+        System.out.println("hello git");
+        System.out.println("hello git2");
+
     }
 
 }
